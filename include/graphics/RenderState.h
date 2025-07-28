9c576130b82d3af73b15560cac8463128a6c0b1a
ChiroYuki <140270407+ChiroYuPy@users.noreply.github.com>
2025-07-28 23:08:30 +0200

    .

---
+++ include/graphics/RenderState.h
@@ -0,0 +1,32 @@
+//
+// Created by ChiroYuki on 24/07/2025.
+//
+
+#ifndef PIXLENGINE_RENDERSTATE_H
+#define PIXLENGINE_RENDERSTATE_H
+
+#include "GLT.h"
+
+class RenderState {
+public:
+    enum class PolygonMode { Fill, Wireframe, Point };
+    enum class PolygonFace { Front, Back, FrontAndBack };
+
+    static void setClearColor(float r, float g, float b, float a);
+
+    static void clear();
+
+    static void enableDepthTest(bool enable);
+
+    static void enableCullFace(bool enable);
+
+    static void setCullFace(PolygonFace face);
+
+    static void setPolygonMode(PolygonFace face, PolygonMode mode);
+
+    static void enableBlend(bool enable);
+
+    static void setBlendFunc();
+};
+
+#endif //PIXLENGINE_RENDERSTATE_H
